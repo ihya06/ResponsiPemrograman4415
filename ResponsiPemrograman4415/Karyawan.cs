169158a4c2873e49c1374f2bccf7ed01dcf94f34
ihya06 <mihyaulumuddin@students.amikom.ac.id>
2022-04-20 09:28:42 +0700

    Add project files.

---
+++ ResponsiPemrograman4415/Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4415
+{
+    internal class Karyawan
+    {
+        public string Nomor { get; set; }
+        public string Nama { get; set; }
+        public float GajiRutin { get; set; }
+        public Karyawan(string nomor, string nama, float gaji)
+        {
+            this.Nomor = nomor;
+            this.Nama = nama;
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+            this.GajiRutin = gaji;
+        }
+
+        public void GajiNaik(float upah)
+        {
+            this.GajiRutin = upah / 100 * this.GajiRutin + this.GajiRutin;
+        }
+    }
+}
---
+++ ResponsiPemrograman4415/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4415
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Karyawan kar1 = new Karyawan("21114415", "Ihya", 5000000);
+            Karyawan kar2 = new Karyawan("21114499", "Udin", 7000000);
+            Console.WriteLine("Program Gaji Bulanan Karyawan Ternak Kode");
+            Console.WriteLine("No   Nomor/Nama        Gaji Setiap Bulan");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", kar1.Nomor, kar1.Nama, kar1.GajiRutin);
+            Console.WriteLine("2    {0}/{1}         {2}", kar2.Nomor, kar2.Nama, kar2.GajiRutin);
+
+            Console.WriteLine();
+            Console.WriteLine("Alhamdulillah Gaji Naik 10%");
+            Console.WriteLine();
+
+            kar1.GajiNaik(10);
+            kar2.GajiNaik(10);
+
+            Console.WriteLine("No   Nomor/Nama        Gaji Setiap Bulan");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", kar1.Nomor, kar1.Nama, kar1.GajiRutin);
+            Console.WriteLine("2    {0}/{1}         {2}", kar2.Nomor, kar2.Nama, kar2.GajiRutin);
+
+            Console.ReadKey();
+        }
+    }
+}
